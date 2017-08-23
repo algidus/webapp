f29435717b85ab334029295d98e338dbfa2d1559
Algidus Leo <algidusleo@gmail.com>
2017-08-23 19:37:50 +0545

    Added Username

--- server.js
+++ server.js
@@ -2,12 +2,6 @@ var app = require("express")();
 var http = require("http").Server(app);
 var io = require("socket.io")(http);
 
-
-//app.get('/', function (req, res){
-//  res.writeHead(200, {"Content-Type": "text/plain"});
-//  res.end("Hello! Welcome to socket demo! \n");
-//});
-
 app.get('/', function(req, res){
 	res.sendFile(__dirname + '/index.html');
 });
